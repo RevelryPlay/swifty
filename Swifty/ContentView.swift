a954575c4d99bfbcdd49d843108f568f7b2fe475
Kylie Benfield <kylie@benfield.design>
2021-05-02 12:10:32 -0500

    Initial Commit

---
+++ Swifty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifty
+//
+//  Created by Kylie Benfield on 5/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swifty/SwiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyApp.swift
+//  Swifty
+//
+//  Created by Kylie Benfield on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
